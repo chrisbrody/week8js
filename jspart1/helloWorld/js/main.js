7e070f4d8301f68871c6b222cc0054a26ebcfb7c
chrisbrody <chrisbrody@codifyacademy.com>
2015-11-21 15:18:25 -0800

    codify class week 8

---
+++ jspart1/helloWorld/js/main.js
@@ -0,0 +1,7 @@
+// Print hello world to console, alert, and to the screen
+// console
+console.log('hello world')
+// screen
+document.getElementById('hello').innerHTML = 'Hello World'
+// alert
+alert('Hello World')
---
+++ jspart2/getInputData/js/main.js
@@ -0,0 +1,8 @@
+var username, password;
+// get user vaule from input
+function getUserData() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('userpassword').value
+
+	console.log(username + " " + password)
+}
